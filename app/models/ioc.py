5c4b07bdba7db55ef38f448df80de823653153f1
jose-seminario <chuquipoma123@gmail.com>
2026-09-14 11:20:33 -0500

    empezando

---
+++ app/models/ioc.py
@@ -0,0 +1,7 @@
+class IOC:
+    def __init__(self, valor, tipo):
+        self.valor = valor
+        self.tipo = tipo
+        self.fecha = None
+        self.score = None
+        self.nivel_riesgo = None
---
+++ app/models/prueba_ioc.py
@@ -0,0 +1,6 @@
+from ioc import IOC
+
+mi_ioc = IOC("8.8.8.8", "IP")
+
+print(mi_ioc.valor)
+print(mi_ioc.tipo)
