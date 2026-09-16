e36101eab8f21f7464ab8b8859aaf4241c9623db
jose-seminario <chuquipoma123@gmail.com>
2026-09-16 15:45:55 -0500

    add DetectorIOC with regex-based type detection

--- detector.py
+++ detector.py
@@ -2,8 +2,8 @@ import re
 class DetectorIOC:
 
     patterns = {
-            'ip':     re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'),
-            'domain': re.compile(r'^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$'),
+            'ip': re.compile(r'^((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)$'),
+            'domain': re.compile(r'^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$'),
             'hash':   re.compile(r'^[a-fA-F0-9]{32}$|^[a-fA-F0-9]{40}$|^[a-fA-F0-9]{64}$')
     }
     def detectar(self, ioc):
